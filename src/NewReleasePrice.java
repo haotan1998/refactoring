f7f4b7712e0033a218d3d7308171b6826fb1a3eb
tanbox08 <tanbox@live.com>
2018-12-14 21:18:35 +0800

    Replace Type Code With State/Strategy

--- src/NewReleasePrice.java
+++ src/NewReleasePrice.java
@@ -2,4 +2,13 @@ public class NewReleasePrice extends Price {
     public int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    @Override
+    double getCharge(int daysRented) {
+        //determine amounts for each line
+        double result = 0;
+        result += daysRented * 3;
+
+        return result;
+    }
 }
