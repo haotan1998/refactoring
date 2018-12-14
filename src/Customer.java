317d490dcb768c9997e09553d67c184bfa6f91dd
tanbox08 <tanbox@live.com>
2018-12-14 17:26:52 +0800

    Removing Temps

--- src/Customer.java
+++ src/Customer.java
@@ -17,16 +17,9 @@ class Customer {
     }
 
     public String statement() {
-        int frequentRenterPoints = 0;
         StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
 
         for (Rental each : _rentals) {
-
-            // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((each.getPriceCode() == Movie.NEW_RELEASE)
-                    && each.getDaysRented() > 1) frequentRenterPoints++;
 
             //show figures for this rental
             result.append("\t").append(each.getTitle());
@@ -38,14 +31,52 @@ class Customer {
         //add footer lines
         result.append("Amount owed is ").append(String.valueOf(getTotalCharge()));
         result.append("\n");
-        result.append("You earned ").append(String.valueOf(frequentRenterPoints));
+        result.append("You earned ").append(String.valueOf(getTotalFrequentRenterPoints()));
         result.append(" frequent renter points");
         return result.toString();
     }
 
-    public double getTotalCharge() { double result = 0;
+    public int getTotalFrequentRenterPoints() {
+        int frequentRenterPoints = 0;
         for (Rental each : _rentals) {
-            result += each.getCharge(); }
-        return result; }
+
+            // add frequent renter points
+            frequentRenterPoints++;
+            // add bonus for a two day new release rental
+            if ((each.getPriceCode() == Movie.NEW_RELEASE)
+                    && each.getDaysRented() > 1) frequentRenterPoints++;
+
+
+        }
+        return frequentRenterPoints;
+    }
+
+
+    public double getTotalCharge() {
+        double result = 0;
+        for (Rental each : _rentals) {
+            result += each.getCharge();
+        }
+        return result;
+    }
+
+    public String htmlStatement() {
+        StringBuilder result = new StringBuilder("<H1>Rentals for <EM>").append(getName()); result.append("</EM></H1><P>\n");
+        for (Rental each : _rentals) {
+        //show figures for each rental
+            result.append(each.getMovie().getTitle()).append(": ");
+            result.append(String.valueOf(each.getCharge())).append("<BR>\n");
+        }
+        //add footer lines
+        result.append("<P>You owe <EM>" + String.valueOf(getTotalCharge()));
+        result.append("</EM><P>\n");
+        result.append("On this rental you earned <EM>");
+        result.append(String.valueOf(getTotalFrequentRenterPoints()));
+        result.append("</EM> frequent renter points<P>");
+
+        return result.toString();
+    }
 
 }
+
+
--- src/Rental.java
+++ src/Rental.java
@@ -2,6 +2,10 @@ class Rental {
     private Movie _movie;
     private int _daysRented;
 
+    public Movie getMovie() {
+        return _movie;
+    }
+
     public Rental(Movie movie, DataRange dataRange) {
         _movie = movie;
         _daysRented = (int)((dataRange.getEnd().getTime() - dataRange.getStart().getTime()) / (1000 * 60 * 60 * 24));
@@ -20,25 +24,6 @@ class Rental {
     }
 
     public double getCharge() {
-        //determine amounts for each line
-        double thisAmount = 0;
-        switch (getPriceCode()) {
-        case Movie.REGULAR:
-            thisAmount += 2;
-            if (getDaysRented() > 2) {
-                thisAmount += (getDaysRented() - 2) * 1.5;
-            }
-            break;
-        case Movie.NEW_RELEASE:
-            thisAmount += getDaysRented() * 3;
-            break;
-        case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (getDaysRented() > 3) {
-                thisAmount += (getDaysRented() - 3) * 1.5;
-            }
-            break;
-        }
-        return thisAmount;
+        return _movie.getCharge(_daysRented);
     }
 }
